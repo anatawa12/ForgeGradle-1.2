750375ab32dd5e3c60c40b0c57f624c5c2cd74a5
anatawa12 <anatawa12@icloud.com>
2019-11-02 15:03:43 +0900

    remove tests for unsupported version format.

--- src/test/java/net/minecraftforge/gradle/ExtensionForgeVersionTest.java
+++ src/test/java/net/minecraftforge/gradle/ExtensionForgeVersionTest.java
@@ -28,56 +28,6 @@ public class ExtensionForgeVersionTest {
     // Invalid version notation! The following are valid notations. Buildnumber, version, version-branch, mcversion-version-branch, and pomotion (sic)
 
     @Test
-    public void testValidBuildNumber() {
-        // buildnumber
-        this.ext.setVersion("965");
-        assertEquals(this.ext.getVersion(), "1.6.4");
-        assertEquals(this.ext.getApiVersion(), "1.6.4-9.11.1.965");
-    }
-
-    @Test
-    public void testValidPromotion() {
-        // promotion
-        this.ext.setVersion("1.6.4-recommended");
-        assertEquals(this.ext.getVersion(), "1.6.4");
-        assertEquals(this.ext.getApiVersion(), "1.6.4-9.11.1.1345");
-    }
-
-// branched promotions ahve been deprecated
-//    @Test
-//    public void testValidPromotionWithBranch()
-//    {
-//        // promotion (with branch)
-//        this.ext.setVersion("1.7.10-latest-new");
-//        assertEquals(this.ext.getVersion(), "1.7.10");
-//        assertEquals(this.ext.getApiVersion(), "1.7.10-10.13.1.1216-new");
-//    }
-
-    @Test
-    public void testValidBuildNumberNoBranch() {
-        // buildnumber (no branch)
-        this.ext.setVersion("1256");
-        assertEquals(this.ext.getVersion(), "1.7.10");
-        assertEquals(this.ext.getApiVersion(), "1.7.10-10.13.2.1256");
-    }
-
-    @Test
-    public void testValidBuildNumberWithBranch() {
-        // buildnumber (with branch)
-        this.ext.setVersion("1257");
-        assertEquals(this.ext.getVersion(), "1.8");
-        assertEquals(this.ext.getApiVersion(), "1.8-11.14.0.1257-1.8");
-    }
-
-    @Test
-    public void testValidVersion() {
-        // version
-        this.ext.setVersion("10.13.2.1256");
-        assertEquals(this.ext.getVersion(), "1.7.10");
-        assertEquals(this.ext.getApiVersion(), "1.7.10-10.13.2.1256");
-    }
-
-    @Test
     public void testValidMcVersionWithVersion() {
         // mcversion-version
         this.ext.setVersion("1.7.10-10.13.2.1256");
@@ -86,51 +36,11 @@ public class ExtensionForgeVersionTest {
     }
 
     @Test
-    public void testValidVersionWithBranch() {
-        // version-branch
-        this.ext.setVersion("11.14.0.1257-1.8");
-        assertEquals(this.ext.getVersion(), "1.8");
-        assertEquals(this.ext.getApiVersion(), "1.8-11.14.0.1257-1.8");
-    }
-
-    @Test
     public void testValidMcVersionWithVersionAndBranch() {
         // mcversion-version-branch
         this.ext.setVersion("1.8-11.14.0.1257-1.8");
         assertEquals(this.ext.getVersion(), "1.8");
         assertEquals(this.ext.getApiVersion(), "1.8-11.14.0.1257-1.8");
-    }
-
-    // Invalid formats
-
-    @Test(expected = GradleConfigurationException.class)
-    public void testInvalidBuild() {
-        // 1.8 build skipped due to 1.7.10
-        this.ext.setVersion("11.14.0.1256-1.8");
-    }
-
-    @Test(expected = GradleConfigurationException.class)
-    public void testInvalidBuildWithMcVersion() {
-        // 1.8 build skipped due to 1.7.10 (with MC version)
-        this.ext.setVersion("1.8-11.14.0.1256-1.8");
-    }
-
-    @Test(expected = GradleConfigurationException.class)
-    public void testInvalidMcVersion() {
-        // invalid MC version
-        this.ext.setVersion("1.7.10-9.11.1.965");
-    }
-
-    @Test(expected = GradleConfigurationException.class)
-    public void testInvalidMcVersionWithBranch() {
-        // invalid MC version (with branch)
-        this.ext.setVersion("1.7.10-11.14.0.1257-1.8");
-    }
-
-    @Test(expected = GradleConfigurationException.class)
-    public void testInvalidBranch() {
-        // invalid branch
-        this.ext.setVersion("1.7.10-11.14.0.1256-1.8");
     }
 
     @Test
