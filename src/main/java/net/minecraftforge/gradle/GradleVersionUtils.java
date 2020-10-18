01df4af58b1169ed6eafb55a982f8a051d2527fc
anatawa12 <anatawa12@icloud.com>
2020-10-18 16:28:28 +0900

    use ObjectFactory.domainObjectSet(Class) instead of DefaultDomainObjectSet.<init>(Class) if possible

--- src/main/java/net/minecraftforge/gradle/GradleVersionUtils.java
+++ src/main/java/net/minecraftforge/gradle/GradleVersionUtils.java
@@ -31,4 +31,23 @@ public class GradleVersionUtils {
             action.run();
         }
     }
+    /**
+     * same version includes after
+     * @param project detect by version of gradle of this project
+     * @param versionName includes this version
+     */
+    public static <T> T choose(Project project, String versionName, Callable<? extends T> before, Callable<? extends T> after) {
+        VersionNumber gradleVersion = VersionNumber.parse(project.getGradle().getGradleVersion());
+        VersionNumber version = VersionNumber.parse(versionName);
+
+        if (gradleVersion.compareTo(version) < 0) {
+            return before.call();
+        } else {
+            return after.call();
+        }
+    }
+
+    public interface Callable<T> {
+        T call();
+    }
 }
