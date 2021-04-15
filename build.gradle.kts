2d764e3fd4df639389f70f76c400e4446e60c511
anatawa12 <anatawa12@icloud.com>
2021-04-15 22:16:15 +0900

    use Srg2Source 4.2.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -82,7 +82,7 @@ dependencies {
     // mcp stuff
     compile("de.oceanlabs.mcp:RetroGuard:3.6.6")
     compile("de.oceanlabs.mcp:mcinjector:3.2-SNAPSHOT")
-    compile("net.minecraftforge.srg2source:Srg2Source:4.0-SNAPSHOT")
+    compile("net.minecraftforge:Srg2Source:4.2.7")
 
     // stupid maven
     deployerJars("org.apache.maven.wagon:wagon-ssh:3.4.3")
